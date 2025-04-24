3492009fc73948474497d6df14502afb4d80c5d2
userTany0013 <tany1ghom@gmail.com>
2025-04-24 19:18:04 +0300

    Error(submitData.get() got an unexpected keyword argument 'id')

--- pereval/add_pereval/views.py
+++ pereval/add_pereval/views.py
@@ -18,13 +18,11 @@ class submitData(APIView):
             return Response({'status': 'success', 'data': serializer.data}, status=status.HTTP_201_CREATED)
         return Response({'status': 'error', 'errors': serializer.errors}, status=status.HTTP_400_BAD_REQUEST)
 
-    def GET(self, request, pk):
+    def get(self, request, pk):
         obj = Pereval.objects.get(id=pk)
-        return Response({
-            'pereval_info': Pereval_InfoSerializer(obj).data
-        })
+        return Response(Pereval_InfoSerializer(obj).data)
 
-    def PATCH(self, request, pk):
+    def patch(self, request, pk):
         pereval_obj = Pereval.objects.get(id=pk)
         serializer = Pereval_UpdateSerializer(pereval_obj, data=request.data)
         if serializer.is_valid():
